779b093d595c5d1c3ff13bfa6e9644c3b3169e36
ilya Kupriyanov <ilya-kyp@yandex.ru>
2022-10-02 17:00:56 +0300

    "кжширвоаине метаданных"

--- DI_Lesson/Model/Container.cs
+++ DI_Lesson/Model/Container.cs
@@ -1,5 +1,7 @@
 ﻿using DI_Lesson.Model.Descriptors;
 using DI_Lesson.Model.Interfaces;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
 using System.Reflection;
 
 namespace DI_Lesson.Model;
@@ -20,38 +22,49 @@ public class Container : IContainer
         }
     }
 
-    private Dictionary<Type, ServiceDescriptorBase> _descriptors;
+    private ImmutableDictionary<Type, ServiceDescriptorBase> _descriptors;
+    private ConcurrentDictionary<Type, Func<IScope, object>> _builtActivators = new();
+
     public Container(IEnumerable<ServiceDescriptorBase> descriptors)
     {
-        _descriptors = descriptors.ToDictionary(x => x.ServiceType);
+        _descriptors = descriptors.ToImmutableDictionary(x => x.ServiceType);
     }
     public IScope CreateScope()
     {
         return new Scope(this);
     }
-    private object CreateInstance(Type service, IScope scope)
+
+    private Func<IScope, object> BuildActivation(Type service)
     {
         if (!_descriptors.TryGetValue(service, out var descriptor))
             throw new InvalidOperationException($"serivice {service} is not registered");
-        
+
         if (descriptor is InstanceBasedServiceDescriptor ib)
-            return ib.Instance;
+            return _ => ib.Instance;
         if (descriptor is FactoryBasedServiceDescriptor fb)
-            return fb.Factory(scope);
+            return fb.Factory;
 
         var tb = (TypeBasedServiceDescriptor)descriptor;
 
-        //сделаем вид, что у нас только один конструктор
         var ctor = tb.ImplementationType.GetConstructors(BindingFlags.Public | BindingFlags.Instance).Single();
         var args = ctor.GetParameters();
-        var parameters = new object?[args.Length];
 
-        for (int i = 0; i < args.Length; i++)
+        return scope =>
         {
-            parameters[i] = CreateInstance(args[i].ParameterType, scope);
-        }
+            var parameters = new object?[args.Length];
 
-        return ctor.Invoke(parameters);
+            for (int i = 0; i < args.Length; i++)
+            {
+                parameters[i] = CreateInstance(args[i].ParameterType, scope);
+            }
+            
+            return ctor.Invoke(parameters);
+        };
+    }
 
+    private object CreateInstance(Type service, IScope scope)
+    {
+        // что бы каждый раз не лазить по метаданным объекта, данные будут находиться в словаре _builtActivators
+        return _builtActivators.GetOrAdd(service, BuildActivation)(scope);
     }
 }

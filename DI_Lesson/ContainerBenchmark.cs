937952d9c763a6fd61c634c038c92d7c783725e5
ilya Kupriyanov <ilya-kyp@yandex.ru>
2022-10-09 15:30:34 +0300

    Добавление пакетов MSDI И Autofac. Сравнение их, используя бенчмарк

--- DI_Lesson/ContainerBenchmark.cs
+++ DI_Lesson/ContainerBenchmark.cs
@@ -1,40 +1,73 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using Autofac;
+using BenchmarkDotNet.Attributes;
 using DI_Lesson.Extensions;
 using DI_Lesson.Model;
 using DI_Lesson.Model.Builders;
 using DI_Lesson.Model.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DI_Lesson;
 
+[MemoryDiagnoser]
 public class ContainerBenchmark
 {
-    private readonly IScope _reflectionBased, _lambdaBased;
+    private readonly IScope _reflectionBasedScope, _lambdaBasedScope;
+	private readonly ILifetimeScope _lifetimeScope;
+	private readonly IServiceScope _serviceScope;
 
 	public ContainerBenchmark()
 	{
-		var lambdaBasedBuilder = new ContainerBuilder(new LambdaBasedActivationbuilder());
-		var reflectionBasedBuilder = new ContainerBuilder(new ReflectionBasedActivationBuilder());
+		var lambdaBasedBuilder = new Model.ContainerBuilder(new LambdaBasedActivationbuilder());
+		var reflectionBasedBuilder = new Model.ContainerBuilder(new ReflectionBasedActivationBuilder());
 
 		InitContainer(lambdaBasedBuilder);
 		InitContainer(reflectionBasedBuilder);
 
-		_reflectionBased = reflectionBasedBuilder.Build().CreateScope();
-		_lambdaBased = reflectionBasedBuilder.Build().CreateScope();
+		_reflectionBasedScope = reflectionBasedBuilder.Build().CreateScope();
+		_lambdaBasedScope = reflectionBasedBuilder.Build().CreateScope();
+
+		_lifetimeScope = InitAutofac();
+		_serviceScope = InitMsdi();
+
 
     }
+
+	private IServiceScope InitMsdi()
+	{
+		var collection = new ServiceCollection();
+		collection.AddTransient<IService, Service>()
+			.AddTransient<Controller, Controller>();
+
+		return collection.BuildServiceProvider().CreateScope();
+	}
+
+	private ILifetimeScope InitAutofac()
+	{
+		var containerBuilder = new Autofac.ContainerBuilder();
+		containerBuilder.RegisterType<Service>().As<IService>();
+		containerBuilder.RegisterType<Controller>().AsSelf();
+
+		return containerBuilder.Build().BeginLifetimeScope();
+	}
 
 	private void InitContainer(IContainerBuilder builder)
 	{
 		builder.RegisterTransient<IService, Service>()
-			.RegisterScoped<Controller, Controller>();
+			.RegisterTransient<Controller, Controller>();
 	}
 
 	[Benchmark(Baseline = true)]
 	public Controller Create() => new Controller(new Service());
 
     [Benchmark]
-    public Controller Reflection() => (Controller)_reflectionBased.Resolve(typeof(Controller));
+    public Controller Reflection() => (Controller)_reflectionBasedScope.Resolve(typeof(Controller));
 
     [Benchmark]
-    public Controller Lambda() => (Controller)_lambdaBased.Resolve(typeof(Controller));
+    public Controller Lambda() => (Controller)_lambdaBasedScope.Resolve(typeof(Controller));
+
+	[Benchmark]
+	public Controller Autofac() => _lifetimeScope.Resolve<Controller>();
+
+	[Benchmark]
+	public Controller Msdi() => _serviceScope.ServiceProvider.GetRequiredService<Controller>();
 }
